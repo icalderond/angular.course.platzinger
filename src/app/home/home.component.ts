b7f9afd67b65f825ce8b1412f06778090873fd7c
Israe Calderón <icalderond@outlook.com>
2020-07-05 21:53:36 -0500

    Usando directiva ngFor

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../interfaces/user'
 
 @Component({
   selector: 'app-home',
@@ -6,8 +7,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  friends: User[];
+  constructor() {
+    let usuario1: User = {
+      nick: 'Eduardo',
+      age: 24,
+      email: 'ed@aoe.aoe',
+      friend: true,
+      uid: 1
+    };
+    let usuario2: User = {
+      nick: 'Freddy',
+      age: 28,
+      email: 'fred@aoe.aoe',
+      friend: true,
+      uid: 1
+    };
+    let usuario3: User = {
+      nick: 'Yuliana',
+      age: 18,
+      email: 'yuli@aoe.aoe',
+      friend: true,
+      uid: 1
+    };
+    let usuario4: User = {
+      nick: 'Ricardo',
+      age: 17,
+      email: 'rick@aoe.aoe',
+      friend: false,
+      uid: 1
+    };
+    let usuario5: User = {
+      nick: 'Marcos',
+      age: 30,
+      email: 'marcos@aoe.aoe',
+      friend: false,
+      uid: 1
+    };
 
-  constructor() { }
+    this.friends = [usuario1, usuario2, usuario3, usuario4, usuario5];
+  }
 
   ngOnInit(): void {
   }
