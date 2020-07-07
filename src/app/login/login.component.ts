5df4466437e776a5e12c042b33635b60a7f37d34
Israe Calderón <icalderond@outlook.com>
2020-07-07 05:36:36 -0500

    Login con correo y clave en firebase

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from '../services/authentication.service';
 
 @Component({
   selector: 'app-login',
@@ -8,10 +9,38 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
 
   operation: string = 'login';
-
-  constructor() { }
+  Email: string = null;
+  Password: string = null;
+  constructor(private authenticationService: AuthenticationService) { }
 
   ngOnInit(): void {
   }
 
+  logIn() {
+    console.log("Email", this.Email);
+    console.log("Password", this.Password);
+
+    this.authenticationService.loginWithEmail(this.Email, this.Password)
+      .then((data) => {
+        alert('Accedió correctamente');
+        console.log(data);
+      }).catch((err) => {
+        alert('Ocurrió un error');
+        console.log(err);
+      });
+  }
+
+  register() {
+    console.log("Email", this.Email);
+    console.log("Password", this.Password);
+
+    this.authenticationService.registerWithEmail(this.Email, this.Password)
+      .then((data) => {
+        alert('Registrado correctamente');
+        console.log(data);
+      }).catch((err) => {
+        alert('Ocurrió un error');
+        console.log(err);
+      });
+  }
 }
